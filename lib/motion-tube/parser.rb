3e7d5b531f047577c0bfd321165bec599a3b9c2e
David Ruan <ruanwz@gmail.com>
2015-03-04 13:29:28 +0800

    query playlist

--- lib/motion-tube/parser.rb
+++ lib/motion-tube/parser.rb
@@ -1,15 +1,13 @@
 module MotionTube
   class Parser
-    attr_accessor :rest_client, :url, :response
-    def initialize(url)
-      @url = url
-      @rest_client = AFMotion::Client.build("http://www.youtube.com") do
-        header "Accept", "application/json"
-        response_serializer :json
-      end
+    attr_accessor :rest_client, :url, :response, :options
+    def initialize(options = {})
+      self.options = options
+      set_rest_client "http://www.youtube.com"
     end
 
-    def parse(&block)
+    def parse(options, &block)
+      self.options.merge! options
       if block
         get_response(&block)
       else
@@ -20,6 +18,7 @@ module MotionTube
     private
 
     def id
+      url = options[:source][:url]
       if url =~ /(?:v=|youtu\.be\/|youtube\.com\/v\/)([^.|&]+)/
         url.scan(/(?:v=|youtu\.be\/|youtube\.com\/v\/)([^.|&]+)/)[0][0]
       elsif url =~ /\/embed\//
@@ -34,7 +33,7 @@ module MotionTube
     end
 
     def title
-      @response.scan(/title=([^&]+)/)[0][0].unescape_url
+      response.scan(/title=([^&]+)/)[0][0].unescape_url
     end
 
 
@@ -61,17 +60,55 @@ module MotionTube
     end
 
     def get_response(&result_callback)
+      if options[:source][:url]
+        get_video_info_from_id id, &result_callback
+      elsif playlist = options[:source][:playlist]
+        set_rest_client("http://gdata.youtube.com")
+        get_video_info_from_playlist playlist, &result_callback
+      end
+    end
 
-      rest_client.post("get_video_info", video_id: id) do |result|
-        @response = result.body
+    def set_rest_client(url)
+      self.rest_client = AFMotion::Client.build(url) do
+        header "Accept", "application/json"
+        response_serializer :json
+      end
+    end
+
+    def get_video_info_from_playlist(playlist, &result_callback)
+      path = "/feeds/api/playlists/#{playlist}?v=2&alt=json&max-results=50"
+      rest_client.get(path) do |result|
+        self.response = result.body
+
+        hash = result.object
+        entry = hash["feed"]["entry"]
+        youtube_playlist_title = hash["feed"]["title"]["$t"]
+
+        video_list = entry.map do |e|
+          e_hash = {}
+          e_hash["title"] = e["title"]["$t"]
+          e_hash["href"] = e["link"][0]["href"]
+          e_hash
+        end
+
+        result_hash = {}
+        result_hash[youtube_playlist_title] = video_list
+        result_callback.call result_hash
+      end
+    end
+
+    def get_video_info_from_id(video_id, &result_callback)
+      rest_client.post("get_video_info", video_id: video_id) do |result|
+        self.response = result.body
         hash = {
-          id: id,
-          title: title,
-          cover: cover,
-          media: media
+          "id" => id,
+          "title" => title,
+          "cover" => cover,
+          "media" => media
         }
         result_callback.call hash
       end
     end
+
   end
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,25 +1,40 @@
 module MotionTube
   describe Parser do
     @youtube_link = "https://www.youtube.com/watch?v=_S1S1gGS3lU"
-    @parser = Parser.new @youtube_link
+    @youtube_playlist = "PL7QBhjs24ko_dq2Nm1NovIqIAr7kAuWJb"
 
     it "has a rest client of afmotion instance" do
-      @parser.rest_client.class.should.equal AFHTTPRequestOperationManager
+      parser = Parser.new
+      parser.rest_client.class.should.equal AFHTTPRequestOperationManager
     end
 
     it "get youtube info hash from video link" do
-      @parser.parse do |result|
+      parser = Parser.new
+      parser.parse(source: {url: @youtube_link}) do |result|
         @result = result
         resume
       end
       wait_max 10 do
-        @result.keys.should.equal [:id, :title, :cover, :media]
-        @result[:title].should.equal "CoreDataQuery+(CDQ)+In+Action"
-        @result[:cover].should.equal "http://i.ytimg.com/vi/_S1S1gGS3lU/0.jpg"
-        @result[:media].keys.should.equal ["mp4", "webm", "flv", "3gp"]
-        @result[:media]["mp4"].size.should.equal 2
+        @result.keys.should.equal %w[id title cover media]
+        @result["title"].should.equal "CoreDataQuery+(CDQ)+In+Action"
+        @result["cover"].should.equal "http://i.ytimg.com/vi/_S1S1gGS3lU/0.jpg"
+        @result["media"].keys.should.equal ["mp4", "webm", "flv", "3gp"]
+        @result["media"]["mp4"].size.should.equal 2
       end
 
+    end
+
+    it "get youtube playlist info hash from playlist" do
+      parser = Parser.new
+      parser.parse(source: {playlist: @youtube_playlist}) do |result|
+        @result = result
+        resume
+      end
+      wait_max 10 do
+        @result.keys.should.equal ["Alphablocks Series 3"]
+        @result["Alphablocks Series 3"].class.should.equal Array
+        @result["Alphablocks Series 3"].first.keys.should.equal %w{ title href}
+      end
     end
   end
 end
